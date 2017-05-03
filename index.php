558abad4ac3c366828c7f8207fbd94a0af8bef97
athan88 <na.tuinstra@gmail.com>
2017-05-03 12:54:59 +0200

    header

--- index.php
+++ index.php
@@ -26,14 +26,76 @@
     </head>
 
     <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img alt="EenmaalAndermaal logo" src="Images/Site-logo.png" class="sitelogo">
-                </a>
+
+        <nav class="navbar navbar-default">
+            <div class="row">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">
+                         <img alt="Brand" src="Images/Site-logo.png" class="">
+                        </a>
+                        <form class="navbar-form col-sm-8 text-center" role="search">
+                          <div class="form-group">
+                              <input type="text" class="form-control" placeholder="Search">
+                          </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
+        </nav>
+
+        </div class="row">
+            <div class="container">
+
+                <div class="col-md-2">
+
+                </div>
+
+                <div class="col-md-8">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="la.jpg" alt="Los Angeles">
+                            </div>
+
+                            <div class="item">
+                                <img src="chicago.jpg" alt="Chicago">
+                            </div>
+
+                            <div class="item">
+                                <img src="ny.jpg" alt="New York">
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+                    <div class="col-md-2">
+                        
+                    </div>
+
+                </div>
+            </div>
+        <div>
+
         </div>
-    </nav>
     </body>
+
 </html>
