a32cbd1a90c7c679b06716db3f0e1e77812412b3
athan88 <na.tuinstra@gmail.com>
2017-05-03 11:47:25 +0200

    header

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!doctype html>
 
+
 <html lang="en">
+
     <head>
         <meta charset="utf-8">
 
@@ -8,10 +10,6 @@
         <meta name="description" content="EenmaalAndermaal">
         <meta name="author" content="Iproject - Groep 3">
 
-
-
-        <!-- Bootstrap -->
-        <!--link rel="stylesheet" href="css/bootstrap-grid.css" -->
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -22,16 +20,20 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-
         <!-- CSS -->
-        <link rel="stylesheet" href="css/Main.css">
+        <link rel="stylesheet" href="CSS/HomePage.css">
 
     </head>
+
     <body>
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <header>
-                <img src="Images/Site-logo.png" alt="EenmaalAndermaal logo" class="sitelogo">
-            </header>
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="EenmaalAndermaal logo" src="Images/Site-logo.png" class="sitelogo">
+                </a>
+            </div>
         </div>
+    </nav>
     </body>
 </html>
