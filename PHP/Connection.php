fdb3d8a2fbf429619b81d265014a8ee16f3bcf73
athan88 <na.tuinstra@gmail.com>
2017-05-03 11:11:40 +0200

    Images

--- PHP/Connection.php
+++ PHP/Connection.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nathan
- * Date: 3-5-17
- * Time: 10:53
- */
+
+
+$hostname = "192.168.122.222";
+$dbname = "eenmaalandermaal";
+$username = "sa";
+$pw= "1q2w3e4r";
+$connection=NULL;
+
+try {
+    $connection = new PDO ("sqlsrv:server=$hostname;database=$dbname;ConnectionPooling=0", "$username", "$pw");
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::SQLSRV_ATTR_DIRECT_QUERY, true);
+
+    echo "<h1>succes!</h1>";
+
+} catch (PDOException $exception){
+    echo "Error connecting to SQL database <br>";
+    echo "following errors occured:<br>";
+    echo $exception->getMessage();
+    echo "<h1>Please contact your system administrator for further assistance</h1>";
+    exit();
+}
+?>
--- index.php
+++ index.php
@@ -1,66 +1,37 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
 
-    <title>Bootstrap test</title>
-    <meta name="description" content="Bootstrap testsite">
-    <meta name="author" content="Nathan">
-
-
-
-    <!-- Bootstrap -->
-    <!--link rel="stylesheet" href="css/bootstrap-grid.css" -->
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="css/Main.css">
-
-</head>
-
-<body>
+        <title>Bootstrap test</title>
+        <meta name="description" content="EenmaalAndermaal">
+        <meta name="author" content="Iproject - Groep 3">
 
 
 
+        <!-- Bootstrap -->
+        <!--link rel="stylesheet" href="css/bootstrap-grid.css" -->
 
-<?php
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-$hostname = "192.168.122.222";
-$dbname = "eenmaalandermaal";
-$username = "sa";
-$pw= "1q2w3e4r";
-$connection=NULL;
-
-try {
-    $connection = new PDO ("sqlsrv:server=$hostname;database=$dbname;ConnectionPooling=0", "$username", "$pw");
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connection->setAttribute(PDO::SQLSRV_ATTR_DIRECT_QUERY, true);
-
-    echo "<h1>succes!</h1>";
-
-} catch (PDOException $exception){
-    echo "Error connecting to SQL database <br>";
-    echo "following errors occured:<br>";
-    echo $exception->getMessage();
-    echo "<h1>Please contact your system administrator for further assistance</h1>";
-    exit();
-}
-?>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/Main.css">
 
-
-</body>
-
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header>
+                <img src="">
+            </header>
+        </div>
+    </body>
 </html>
