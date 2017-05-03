23a7130ac5dfb9184e10e3f6f85100542a77fec6
athan88 <na.tuinstra@gmail.com>
2017-05-03 15:49:34 +0200

    html frontpage

--- index.php
+++ index.php
@@ -11,14 +11,10 @@
         <meta name="author" content="Iproject - Groep 3">
 
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- bootstrap !-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <!-- CSS -->
         <link rel="stylesheet" href="CSS/HomePage.css">
@@ -27,8 +23,8 @@
 
     <body>
 
+    <div class="row">
         <nav class="navbar navbar-default">
-            <div class="row">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <a class="navbar-brand" href="#">
@@ -42,60 +38,69 @@
                         </form>
                     </div>
                 </div>
-            </div>
         </nav>
+    </div>
 
-        </div class="row">
-            <div class="container">
+    <div class="row well">
+        <div class="container">
 
-                <div class="col-md-2">
-
+            <div class="col-md-2 hvisible-md visible-lg">
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">
+                        Categories
+                    </a>
+                    <a href="#" class="list-group-item">Auto's</a>
+                    <a href="#" class="list-group-item">Electronica</a>
+                    <a href="#" class="list-group-item">Boeken</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
                 </div>
+            </div>
 
-                <div class="col-md-8">
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
+            <div class="col-md-8">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
 
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <img src="la.jpg" alt="Los Angeles">
-                            </div>
-
-                            <div class="item">
-                                <img src="chicago.jpg" alt="Chicago">
-                            </div>
-
-                            <div class="item">
-                                <img src="ny.jpg" alt="New York">
-                            </div>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="Images/ModelS.jpeg" alt="Los Angeles" style="width:100%;">
                         </div>
 
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                        <div class="item">
+                            <img src="Images/ModelX.jpeg" alt="Chicago" style="width:100%;">
+                        </div>
+
+                        <div class="item">
+                            <img src="Images/Model3.jpeg" alt="New york" style="width:100%;">
+                        </div>
                     </div>
 
-                    <div class="col-md-2">
-                        
-                    </div>
-
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
+
+
             </div>
-        <div>
 
+
+            <div class="col-md-2">
+
+            </div>
         </div>
-    </body>
+    </div>
 
+
+    </body>
 </html>
