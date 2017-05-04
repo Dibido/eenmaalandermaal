e439b15ef7fa279086848d497329d8107cff7d2c
Dibido <dibrandokter@hotmail.com>
2017-05-04 15:21:10 +0200

    categoriepagina opzet toegevoegd

---
+++ categorie.php
@@ -0,0 +1,112 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Categorie</title>
+    <meta name="description" content="EenmaalAndermaal">
+    <meta name="author" content="Iproject - Groep 3">
+
+    <!-- bootstrap !-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/categorie.css">
+</head>
+
+<body>
+
+<div class="row">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="Images/Site-logo.png" class="">
+                </a>
+                <form class="navbar-form col-sm-8 text-center" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Zoek</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</div>
+
+<div class="row">
+    <div class="col-md-2">
+        <div class="container-fluid">
+            <div class="visible-lg visible-md visible-sm visible-xs">
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">
+                        Auto onderdelen
+                    </a>
+                    <a href="#" class="list-group-item">Accu's</a>
+                    <a href="#" class="list-group-item">Uitlaatpijpen</a>
+                    <a href="#" class="list-group-item">Remblokken</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="well well-lg col-md-10">
+            <h2>Trending</h2>
+            <div class="well well-sm col-md-2">
+                <h5>Tesla X</h5>
+                <img src="Images/ModelX.jpeg" height="100" width="220">
+                <span class="glyphicon glyphicon-euro" aria-hidden="true">15.00</span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#2</h5>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#3</h5>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#4</h5>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="well well-lg col-md-10">
+            <h2>Trending</h2>
+            <div class="well well-sm col-md-2">
+                <h5>Tesla X</h5>
+                <img src="Images/ModelS.jpeg" height="100" width="220">
+                <span class="glyphicon glyphicon-euro" aria-hidden="true">15.00</span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#2</h5>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#3</h5>
+            </div>
+            <div class="well well-sm col-md-2 col-md-offset-1">
+                <h5>Aanbieding#4</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
+<?php
+//TODO: add database queries and formatting.
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    //TODO: get category
+}
+?>
