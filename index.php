3c48da77fdcc0fc9147c18dd3575d9671b5e8d3a
athan88 <na.tuinstra@gmail.com>
2017-05-06 20:26:33 +0200

    frontpage updates

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 
 
         <!-- bootstrap !-->
+
+        <!--link rel="stylesheet" href="CSS/theme.min.css"-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -19,35 +21,50 @@
         <!-- CSS -->
         <link rel="stylesheet" href="CSS/HomePage.css">
 
+
     </head>
 
     <body>
 
-    <div class="navbar navbar-default navbar-static-top">
+
+    <!-- Navigation -->
+
+    <nav class="navbar navbar-default navbar-static-top">
         <div class="container-fluid">
-            <a href="index.php" class="navbar-brand navbar-left"><img src="Images/Site-logo.png" alt="EenmaalAndermaal logo"></a>
-            <ul class="nav navbar-nav">
-                <li>
-                    <form class="navbar-form navbar-center" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">zoek</button>
-                    </form>
-                </li>
-                <li>
-                    <button type="button" class="btn btn-default navbar-btn"><i class="glyphicon glyphicon-menu-down"></i></button>
-                </li>
-                <li>
-                    <button type="button" class="btn btn-primary navbar-btn">Plaats veiling</button>
-                </li>
-                <li>
-                    <a><i class="glyphicon glyphicon-user"></i></a>
-                </li>
+            <a href="index.php" class="navbar-brand">
+                <img src="Images/Site-logo.png" alt="EenmaalAndermaal Logo">
+            </a>
+
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="zoeken">
+                </div>
+                <button type="submit" class="btn btn-default hidden-sm hidden-xs"><i class="glyphicon glyphicon-search"></i></button>
+                <button type="button" class="btn btn-default"><i class="glyphicon glyphicon-menu-down"></i></button>
+            </form>
+
+                <div class="pull-right">
+                    <ul class="nav navbar-nav collapse navbar-collapse">
+                        <li><button class="btn btn-default navbar-btn hidden-md hidden-lg" data-toggle="collapse" data-target="#MobileButtons"><i class="glyphicon glyphicon-menu-hamburger"></i></button></li>
+                        <li><button class="btn btn-primary navbar-btn hidden-sm hidden-xs">Plaats veiling</button></li>
+                        <li><button class="btn btn-default navbar-btn hidden-sm hidden-xs NavRightButton"><i class="glyphicon glyphicon-user"></i></button></li>
+                    </ul>
+                </div>
+        </div>
+    </nav>
+
+    <!-- Mobile Buttons -->
+
+    <div class="container-fluid collapse text-center" id="MobileButtons" style="font-size: 24px;">
+        <div class="row">
+            <ul class="nav nav-pills nav-stacked">
+                <li><a class="row-md-12" href="#">Plaats veiling</a></li>
+                <li><a class="row-md-12" href="#">Login</a></li>
             </ul>
         </div>
     </div>
 
+    <!--Carousel -->
 
     <div class="container-fluid">
         <div class="row well">
