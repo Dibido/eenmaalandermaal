423312ff082a6999d4bb8298c9e2c1069b99f2f5
Martijn-Vogelaar <martijn.j.vogelaar@gmail.com>
2017-05-08 12:54:04 +0200

    SQL Script aangepast. Run volgorde: FUNCTION-DDL-INSERT Niet werkende functie vervangen door een trigger

---
+++ SQL script/Testplekje.sql
@@ -0,0 +1,9 @@
+--Normaal insert statement zonder aangepaste begin tijd
+INSERT INTO Voorwerp (titel, beschrijving, startprijs, betalingswijze, betalingsinstructie, plaatsnaam,
+                      land, looptijd,  verzendkosten, verzendinstructies, verkoper, koper)
+VALUES ('Testproduct', 'beschrijving', 4, 'Bank / Giro', 'Maak 500 euro over naar NLINGB#030493032039384',
+                       'Arnhem', 'NLD', 3, 14.20, 'test', 'kees', 'tinus')
+
+
+INSERT INTO Bod(voorwerpnummer, bodbedrag, gebruiker) VALUES (1,3,'tiasd')
+
