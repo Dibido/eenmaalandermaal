59fdc8ba2e1f6094acfe27b414dbf65648e63b5b
Luck Matheus Hermsen <luckmatheushermsen@hotmail.com>
2017-05-09 15:47:36 +0200

    Zoek Queries NEW

--- SQL script/ZoekQueries.sql
+++ SQL script/ZoekQueries.sql
@@ -12,12 +12,12 @@ SELECT TOP 50 * FROM Voorwerp  WHERE VW_titel LIKE '%$keyWords%' AND VW_beschrij
 /* Haal Rubrieken op */
 SELECT * FROM Rubriek
 /* Show items gebonden aan Categorie*/
-SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON Voorwerp.VW_voorwerpnummer = Voorwerp_Rubriek.VR_Voorwerp_Nummer
+SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON VW_voorwerpnummer = VR_Voorwerp_Nummer
 /* Zoek in bepaalde Categorie op Titel */
-SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON Voorwerp.VW_voorwerpnummer = Voorwerp_Rubriek.VR_Voorwerp_Nummer
+SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON VW_voorwerpnummer = VR_Voorwerp_Nummer
 WHERE VW_titel LIKE '%$keyWords%' AND VR_Rubriek_Nummer LIKE '95' -- Var voor categorie
 /* Zoek in bepaalde Categorie op Beschrijving */
-SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON Voorwerp.VW_voorwerpnummer = Voorwerp_Rubriek.VR_Voorwerp_Nummer
+SELECT TOP 50 * FROM Voorwerp JOIN Voorwerp_Rubriek ON VW_voorwerpnummer = VR_Voorwerp_Nummer
 WHERE VW_beschrijving LIKE '%keyWords%' AND VR_Rubriek_Nummer LIKE '95' -- Var voor categorie
 
 
@@ -36,4 +36,3 @@ WHERE b.BOD_bodbedrag BETWEEN '$MinPrijs' AND '$MaxPrijs'
 
 
 --(SELECT max(BOD_bodbedrag) AS HuidigBod FROM Bod b)
---test3
