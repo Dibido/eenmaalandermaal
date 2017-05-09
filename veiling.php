76055287d521ddcb171206227c9c80de37116294
athan88 <na.tuinstra@gmail.com>
2017-05-09 15:57:36 +0200

    ontwerp advertentei

---
+++ veiling.php
@@ -0,0 +1,46 @@
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+
+        <title>Bootstrap test</title>
+        <meta name="description" content="EenmaalAndermaal">
+        <meta name="author" content="Iproject - Groep 3">
+
+
+        <!-- bootstrap !-->
+
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="CSS/theme.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="CSS/HomePage.css">
+        <link rel="stylesheet" href="CSS/Voorwerp.css">
+        <link rel="stylesheet" href="CSS/navigation.css">
+
+
+    </head>
+
+    <body>
+
+        <div class="veiling">
+            <div class="veiling-titel">
+                <p>Gratis Model S</p>
+            </div>
+            <div class="veiling-image">
+                <img>
+            </div>
+            <div class="veiling-prijs-tijd">
+
+            </div>
+            <div class="veiling-rating-bied">
+
+            </div>
+        </div>
+
+    </body>
+
+</html>
