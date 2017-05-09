fec5d7c8e6a3106c2d3505989f7b91fd9ddc30f6
athan88 <na.tuinstra@gmail.com>
2017-05-09 16:58:11 +0200

    veiling.php

--- veiling.php
+++ veiling.php
@@ -1,46 +1,58 @@
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
 
-        <title>Bootstrap test</title>
-        <meta name="description" content="EenmaalAndermaal">
-        <meta name="author" content="Iproject - Groep 3">
-
-
-        <!-- bootstrap !-->
+    <title>Bootstrap test</title>
+    <meta name="description" content="EenmaalAndermaal">
+    <meta name="author" content="Iproject - Groep 3">
 
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="CSS/theme.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="CSS/HomePage.css">
-        <link rel="stylesheet" href="CSS/Voorwerp.css">
-        <link rel="stylesheet" href="CSS/navigation.css">
+    <!-- bootstrap !-->
 
 
-    </head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/theme.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <body>
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/HomePage.css">
+    <link rel="stylesheet" href="CSS/navigation.css">
+    <link rel="stylesheet" href="CSS/veiling.css">
 
-        <div class="veiling">
-            <div class="veiling-titel">
-                <p>Gratis Model S</p>
-            </div>
-            <div class="veiling-image">
-                <img>
-            </div>
-            <div class="veiling-prijs-tijd">
 
-            </div>
-            <div class="veiling-rating-bied">
+</head>
 
-            </div>
+<body>
+
+<div class="veiling">
+    <div class="veiling-titel label label-info">
+        Gratis Model S
+    </div>
+    <div class="veiling-image" style="background-image:url(Images/21-9.jpg)"></div>
+    <div class="veiling-prijs-tijd">
+        <div class="prijs label label-default"><i class="glyphicon glyphicon-euro"></i> 150000</div>
+        <div class="tijd label label-default">1:15:25 <i class="glyphicon glyphicon-time"></i></div>
+    </div>
+
+    <div class="veiling-rating-bied">
+        <div class="rating label label-default">
+            <i class="glyphicon glyphicon-star"></i>
+            <i class="glyphicon glyphicon-star"></i>
+            <i class="glyphicon glyphicon-star"></i>
+            <i class="glyphicon glyphicon-star"></i>
+            <i class="glyphicon glyphicon-star-empty"></i>
         </div>
+        <div>
+            <button class="btn btn-primary bied">Bied Nu!</button>
+        </div>
+    </div>
 
-    </body>
+</div>
+
+
+
+</body>
 
 </html>
