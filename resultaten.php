44afeb4d4c105f96db04e3310fb4b59094b1643a
Luck Matheus Hermsen <luckmatheushermsen@hotmail.com>
2017-05-12 14:54:56 +0200

    resultaten.php UPDATE

--- resultaten.php
+++ resultaten.php
@@ -1,26 +1,3 @@
-<?php
-require 'PHP/connection.php';
-
-    if(!empty($_GET['zoekterm'])){
-        $zoekterm = $_GET['zoekterm'];
-            //bouwen query
-        $sql = "SELECT TOP 50 * FROM Voorwerp  WHERE VW_titel LIKE '%$zoekterm%'";
-        $result = $connection->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-        print_r($result);
-        //outputRows($results);
-    }
-
-function outputRows($results){
-    while ( $row = $results->fetch( PDO::FETCH_ASSOC )){
-            echo "<ul><pre><li>{$row['VW_voorwerpnummer']}'>{$row['VW_titel']} ({$row['VW_startprijs']})</li></pre></ul>";
-        }
-    return;
-}
-
-?>
-
-
-
 <!doctype html>
 
 <html lang="en">
@@ -36,10 +13,11 @@ function outputRows($results){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <!-- price slider --> 
-   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.css">
+    <!-- price slider -->
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/bootstrap-slider.js"></script>
-    
+
 
     <!-- CSS -->
     <link rel="stylesheet" href="CSS/categorie-old.css">
@@ -65,7 +43,9 @@ function outputRows($results){
             </div>
             <button type="submit" class="btn btn-default hidden-sm hidden-xs"><i class="glyphicon glyphicon-search"></i>
             </button>
-            <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#AdvancedSearch">Advanced Search</button>
+            <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#AdvancedSearch">Advanced
+                Search
+            </button>
         </form>
 
         <div class="pull-right">
@@ -79,7 +59,7 @@ function outputRows($results){
                 </li>
                 <li>
                     <button class="btn btn-default navbar-btn hidden-sm hidden-xs NavRightButton"><i
-                            class="glyphicon glyphicon-user"></i></button>
+                                class="glyphicon glyphicon-user"></i></button>
                 </li>
             </ul>
         </div>
@@ -104,40 +84,51 @@ function outputRows($results){
             <div class="list-group">
                 <a href="#" class="list-group-item active">Opties</a>
 
-                <a href="#" class="list-group-item">Prijs: <b>€ 10 - € 1000</b> <input id="pslider" type="text" class="span2" value="" data-slider-min="10" data-slider-max="1000" data-slider-step="5" data-slider-value="[150,450]"/> </a>
+                <a href="#" class="list-group-item">Prijs: <b>€ 10 - € 1000</b> <input id="pslider" type="text"
+                                                                                       class="span2" value=""
+                                                                                       data-slider-min="10"
+                                                                                       data-slider-max="1000"
+                                                                                       data-slider-step="5"
+                                                                                       data-slider-value="[150,450]"/>
+                </a>
 
-                 <a href="#" class="list-group-item">Rating: <b>1</b> 
+                <a href="#" class="list-group-item">Rating: <b>1</b>
                     <select>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
                     </select>
- 
-                <a href="#" class="list-group-item">Afstand: <b>10KM - 100KM</b> <input id="aslider" type="text" class="span2" value="" data-slider-min="10" data-slider-max="500" data-slider-step="5" data-slider-value="[10,100]"/>
-                 </a>
+
+                    <a href="#" class="list-group-item">Afstand: <b>10KM - 100KM</b> <input id="aslider" type="text"
+                                                                                            class="span2" value=""
+                                                                                            data-slider-min="10"
+                                                                                            data-slider-max="500"
+                                                                                            data-slider-step="5"
+                                                                                            data-slider-value="[10,100]"/>
+                    </a>
 
                     <script>
-                    var slider = new Slider('#pslider', {});
-                    var slider = new Slider('#aslider', {});
+                        var slider = new Slider('#pslider', {});
+                        var slider = new Slider('#aslider', {});
                     </script>
-                    <a href="#" class="list-group-item active" style="background-color: #524bab; text-align: center;")>
-                    Aanpassen
-                </a>
-               
-            </div>
-            
+                    <a href="#" class="list-group-item active" style="background-color: #524bab; text-align: center;" )>
+                        Aanpassen
+                    </a>
 
-                <a href="#" class="list-group-item active" id="Header-Categories">
-                    Categorieën
-                </a>
-                <a href="#" class="list-group-item">Auto's</a>
-                <a href="#" class="list-group-item">Electronica</a>
-                <a href="#" class="list-group-item">Boeken</a>
-                <a href="#" class="list-group-item">Vestibulum at eros</a>
-                <a href="#" class="list-group-item">Electronica</a>
-                <a href="categorie.php" class="list-group-item active text-center">Meer catogorieën <i
+            </div>
+
+
+            <a href="#" class="list-group-item active" id="Header-Categories">
+                Categorieën
+            </a>
+            <a href="#" class="list-group-item">Auto's</a>
+            <a href="#" class="list-group-item">Electronica</a>
+            <a href="#" class="list-group-item">Boeken</a>
+            <a href="#" class="list-group-item">Vestibulum at eros</a>
+            <a href="#" class="list-group-item">Electronica</a>
+            <a href="categorie.php" class="list-group-item active text-center">Meer catogorieën <i
                         class="text-right glyphicon glyphicon-plus-sign"></i></a>
         </div>
     </div>
@@ -145,112 +136,101 @@ function outputRows($results){
         <ol class="breadcrumb">
             <li><a href="index.php">Home</a></li>
             <li class="active">Resultaten</li>
-                <div class="pull-right" >
-                    <b>Sorteer:</b>
-                    <select id="example">
-                      <option value="1">Tijd: nieuw aangeboden</option>
-                      <option value="2">Tijd: eerst afgelopen</option>
-                      <option value="3">Prijs: laagste bovenaan</option>
-                      <option value="4">Prijs: hoogste bovenaan</option>
-                      <option value="5">Afstand: dichtstbijzijnde eerst</option>
-                    </select>
-                 </div>   
-        </ol>   
+            <div class="pull-right">
+                <b>Sorteer:</b>
+                <select id="example">
+                    <option value="1">Tijd: nieuw aangeboden</option>
+                    <option value="2">Tijd: eerst afgelopen</option>
+                    <option value="3">Prijs: laagste bovenaan</option>
+                    <option value="4">Prijs: hoogste bovenaan</option>
+                    <option value="5">Afstand: dichtstbijzijnde eerst</option>
+                </select>
+            </div>
+        </ol>
     </div>
 
     <!-- Trending items -->
 
-    <d iv class="well well-sm col-md-9 pull-left">
+    <div class="well well-sm col-md-9 pull-left">
         <h2>Resultaten</h2>
         <!-- test -->
 
-
-    <div class="well well-sm">
-        <strong>Display</strong>
-        <div class="btn-group">
-            <a href="#" id="list" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-th-list">
+        <div class="well well-sm">
+            <strong>Display</strong>
+            <div class="btn-group">
+                <a href="#" id="list" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-th-list">
             </span>List</a> <a href="#" id="grid" class="btn btn-default btn-sm"><span
-                class="glyphicon glyphicon-th"></span>Grid</a>
+                            class="glyphicon glyphicon-th"></span>Grid</a>
+            </div>
         </div>
-    </div>
-    <div id="products" class="row list-group">
-        <div class="item  col-xs-4 col-lg-4">
-            <div class="thumbnail">
-                <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
-                <div class="caption">
-                    <h4 class="group inner list-group-item-heading">
-                        Product title</h4>
-                    <p class="group inner list-group-item-text">
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                    <div class="row">
-                        <div class="col-xs-12 col-md-6">
-                            <p class="lead">
-                                $21.000</p>
-                        </div>
-                        <div class="col-xs-12 col-md-6">
-                            <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
-                        </div>
+
+        <?php
+        require 'PHP/connection.php';
+
+        if (!empty($_GET['zoekterm'])) {
+            $zoekterm = $_GET['zoekterm'];
+            //bouwen query
+            $sql = "SELECT  * FROM Voorwerp v LEFT JOIN Bod b ON v.VW_voorwerpnummer = b.BOD_voorwerpnummer 
+                              WHERE (B.BOD_bodbedrag = (SELECT TOP 1 BOD_Bodbedrag 
+                                                        FROM Bod 
+                                                        WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag 
+                                                                                    FROM Bod WHERE BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) OR b.BOD_bodbedrag IS NULL) 
+                                    AND VW_titel LIKE '%$zoekterm%'";
+            $result = $connection->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            outputRows($result);
+        }
+
+        function outputRows($result)
+        {
+            foreach ($result as $row) {
+                echo "<div class=\"item  col-xs-4 col-lg-4\">
+            <div class=\"veiling thumbnail\">
+                <div class=\"veiling-titel label label-info\">
+                    {$row['VW_titel']}
+                </div>
+                <div class=\"veiling-image\" style=\"background-image:url(images/16-9.jpeg)\"></div>
+                <p>{$row['VW_beschrijving']}</p>
+                <div class=\"veiling-prijs-tijd\">
+                    <div class=\"prijs label label-default\"><i class=\"glyphicon glyphicon-euro\">
+                    150000000
+                    </i>
                     </div>
+                    <div class=\"tijd label label-default\">1:15:25 <i class=\"glyphicon glyphicon-time\"></i></div>
+                </div>
+                <div class=\"veiling-rating-bied label label-default\">
+                    <div class=\"rating text-center\">
+                        <i class=\"glyphicon glyphicon-star\"></i>
+                        <i class=\"glyphicon glyphicon-star\"></i>
+                        <i class=\"glyphicon glyphicon-star\"></i>
+                        <i class=\"glyphicon glyphicon-star\"></i>
+                        <i class=\"glyphicon glyphicon-star-empty\"></i>
+                    </div>
+                    <button class=\"btn btn-primary bied\">Bied Nu!</button>
                 </div>
             </div>
-        </div>
-<div class="item  col-xs-4 col-lg-4">
-    <div class="veiling thumbnail">
-        <div class="veiling-titel label label-info">
-            Gratis Model S
-        </div>
-        <div class="veiling-image" style="background-image:url(images/16-9.jpeg)"></div>
-        <p >
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-        <div class="veiling-prijs-tijd">
-            <div class="prijs label label-default"><i class="glyphicon glyphicon-euro"></i> 150000</div>
-            <div class="tijd label label-default">1:15:25 <i class="glyphicon glyphicon-time"></i></div>
-        </div>
-        <div class="veiling-rating-bied label label-default">
-            <div class="rating text-center">
-                <i class="glyphicon glyphicon-star"></i>
-                <i class="glyphicon glyphicon-star"></i>
-                <i class="glyphicon glyphicon-star"></i>
-                <i class="glyphicon glyphicon-star"></i>
-                <i class="glyphicon glyphicon-star-empty"></i>
-            </div>
-            <button class="btn btn-primary bied">Bied Nu!</button>
-        </div>
-    </div>
-</div>
-        <div class="item  col-xs-4 col-lg-4">
-            <div class="thumbnail">
-                <img class="group list-group-image" src="images/testimg1.jpg" alt="" />
-                <div class="caption">
-                    <h4 class="group inner list-group-item-heading">
-                        Product title</h4>
-                    <p class="group inner list-group-item-text">
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                    <div class="row">
-                        <div class="col-xs-12 col-md-6">
-                            <p class="lead">
-                                $21.000</p>
-                        </div>
-                        <div class="col-xs-12 col-md-6">
-                            <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div>";
+            }
+        }
+
+        ?>
+
+
     </div>
 </div>
 
-<script> 
-$(document).ready(function() {
-    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
-    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
-});
+<script>
+    $(document).ready(function () {
+        $('#list').click(function (event) {
+            event.preventDefault();
+            $('#products .item').addClass('list-group-item');
+        });
+        $('#grid').click(function (event) {
+            event.preventDefault();
+            $('#products .item').removeClass('list-group-item');
+            $('#products .item').addClass('grid-group-item');
+        });
+    });
 </script>
-
 
 
 </div>
