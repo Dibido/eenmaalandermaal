9ddab583be697f455bd325a01d357edc1f41732f
athan88 <na.tuinstra@gmail.com>
2017-05-15 09:40:29 +0200

    footer

--- index.php
+++ index.php
@@ -6,156 +6,6 @@ require('PHP/SQL-Querries.php');
 
 $response = NULL;
 
-
-/*
- *
- *      Getting the information for all the adverts from the database
- *
- *
- */
-
-
-
-/* Getting the top 2 products that are almost closed en saving in $TopClosed */
-$query = "
-SELECT TOP 2
-  --Vul hier je TOP X hoeveelheid in
-  VW_voorwerpnummer,
-  VW_titel,
-  (SELECT TOP 1 BOD_Bodbedrag FROM Bod WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag FROM Bod WHERE BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) as prijs,
-  DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde) AS tijd,
-  COUNT(*)                                    AS Biedingen,
-  (SELECT TOP 1 BES_filenaam from Bestand WHERE BES_voorwerpnummer = VW_voorwerpnummer) as ImagePath
-FROM Voorwerp
-  INNER JOIN BOD
-    ON Voorwerp.VW_voorwerpnummer = BOD_voorwerpnummer
-WHERE DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde) < 1000 AND DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde) > 2 AND
-      --Vul hier de minimum en maximum tijd over in
-      VW_voorwerpnummer IN (SELECT BOD_voorwerpnummer
-                            FROM Bod
-                              LEFT JOIN Voorwerp_Rubriek
-                                ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
-                              LEFT JOIN Rubriek
-                                ON Rubriek.RB_Nummer = Voorwerp_Rubriek.VR_Rubriek_Nummer
-                              LEFT JOIN Rubriek r1
-                                ON r1.RB_Nummer = Rubriek.RB_Parent
-                              LEFT JOIN Rubriek r2
-                                ON r2.RB_Nummer = r1.RB_Parent
-                            WHERE r2.RB_Naam IN (SELECT TOP 3
-                                                   RB_Naam --Vul hier de top X categoriën in waarvan producten moeten worden laten zien.
-                                                 FROM Rubriek
-                                                   LEFT JOIN
-                                                   (SELECT
-                                                      Rubriek.RB_Parent,
-                                                      aantal
-                                                    FROM Rubriek
-                                                      LEFT JOIN (SELECT
-                                                                   RB_Parent,
-                                                                   COUNT(BOD_voorwerpnummer) AS aantal
-                                                                 FROM Voorwerp_Rubriek
-                                                                   LEFT JOIN Rubriek
-                                                                     ON Rubriek.RB_Nummer =
-                                                                        Voorwerp_Rubriek.VR_Rubriek_Nummer
-                                                                   LEFT JOIN Bod
-                                                                     ON Voorwerp_Rubriek.VR_Voorwerp_Nummer =
-                                                                        Bod.BOD_voorwerpnummer
-                                                                 GROUP BY RB_Parent) eerste
-                                                        ON Rubriek.RB_Volgnummer = eerste.RB_Parent
-                                                    GROUP BY Rubriek.RB_Parent, aantal) tweede
-                                                     ON Rubriek.RB_Volgnummer = tweede.RB_Parent
-                                                 GROUP BY Rubriek.RB_Naam
-                                                 ORDER BY MAX(aantal) DESC)
-                            GROUP BY BOD_voorwerpnummer, r2.RB_Naam)
-GROUP BY VW_voorwerpnummer, VW_looptijdEinde, VW_titel
-ORDER BY Biedingen DESC
-
-";
-
-try {
-    global $response;
-    $response = $connection->query($query)->fetchAll(PDO::FETCH_ASSOC);
-} catch (Exception $e) {
-    echo('<h1>De top 2 producten kunnen niet opgehaald worden</h1>');
-    echo('<p>Error: '. $e->getMessage() . '</p>');
-}
-    $TopClosed = $response;
-
-
-
-/* Getting the top 3 uit belangrijkste categorie, hoogste rating en meeste biedingen en slaat op in $TopCarousel*/
-
-$query = "
-SELECT TOP 3
-  VW_voorwerpnummer,
-  VW_titel,
-  (SELECT TOP 1 BOD_Bodbedrag
-   FROM Bod
-   WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag
-                               FROM Bod
-                               WHERE BOD_voorwerpnummer = VW_voorwerpnummer
-                               ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer
-   ORDER BY BOD_Bodbedrag DESC)                  AS prijs,
-  DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde)    AS tijd,
-  COUNT(*)                                       AS Biedingen,
-  (SELECT TOP 1 BES_filenaam
-   FROM Bestand
-   WHERE BES_voorwerpnummer = VW_voorwerpnummer) AS ImagePath
-FROM Voorwerp
-WHERE VW_voorwerpnummer IN (
-  SELECT DISTINCT BOD_voorwerpnummer
-  --Selecteerd de naam van de Hoofdcategorie per voorwerpnummer
-  FROM Bod
-    INNER JOIN Voorwerp_Rubriek
-      ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
-    INNER JOIN Rubriek
-      ON Rubriek.RB_Nummer = Voorwerp_Rubriek.VR_Rubriek_Nummer
-    INNER JOIN Rubriek r1
-      ON r1.RB_Nummer = Rubriek.RB_Parent
-    INNER JOIN Rubriek r2
-      ON r2.RB_Nummer = r1.RB_Parent
-  WHERE r2.RB_Naam != 'root' AND r2.RB_Naam IN (
-    --Selecteerd de top 1 categoriën door te kijken naar de hoeveelheid biedingen
-    SELECT
-      TOP 1 RB_Naam
-    FROM Rubriek
-      --Lees de de Innerjoins van achter naar voren.
-      INNER JOIN
-      --Selecteerd de tweede laag van de categoriën
-      (SELECT
-         Rubriek.RB_Parent,
-         aantal
-       FROM Rubriek
-         INNER JOIN
-         --Selecteerd de hoeveelheid biedingen per categorie op de onderstelaag
-         (SELECT
-            RB_Parent,
-            COUNT(BOD_voorwerpnummer) AS aantal
-          FROM Voorwerp_Rubriek
-            INNER JOIN Rubriek
-              ON Rubriek.RB_Nummer = Voorwerp_Rubriek.VR_Rubriek_Nummer
-            INNER JOIN Bod
-              ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
-          GROUP BY RB_Parent)
-         eerste ON Rubriek.RB_Volgnummer = eerste.RB_Parent
-       GROUP BY Rubriek.RB_Parent, aantal) tweede ON Rubriek.RB_Volgnummer = tweede.RB_Parent
-    WHERE Rubriek.RB_Parent = 0
-    GROUP BY Rubriek.RB_Naam
-    ORDER BY MAX(aantal) DESC)
-) --TODO AND Verkoper van voorwerp in top van de gebruikerreviews
-GROUP BY VW_voorwerpnummer, VW_titel, VW_looptijdEinde
-";
-
-try {
-    global $response;
-    $response = $connection->query($query)->fetchAll(PDO::FETCH_ASSOC);
-} catch (Exception $e) {
-    echo('<h1>De top 2 producten kunnen niet opgehaald worden</h1>');
-    echo('<p>Error: '. $e->getMessage() . '</p>');
-}
-$TopCarousel = $response;
-
-
-
 ?>
 
 <!doctype html>
@@ -233,7 +83,7 @@ $TopCarousel = $response;
         <form class="navbar-form" action="resultaten.php" method="GET">
             <div class="form-group" style="display:inline;">
                 <div class="input-group" style="display:table;">
-                    <input class="form-control" name="search" placeholder="Search Here" autocomplete="off" autofocus="autofocus" type="text">
+                    <input class="form-control" name="zoekterm" placeholder="Search Here" autocomplete="off" autofocus="autofocus" type="text">
                     <span class="input-group-btn" id="sizing-addon1" style="width:1%;"><button class="btn btn-secondary" type="submit" style="background-color: #ffffff; border-color: #f2f2f2;"><span class="glyphicon glyphicon-search"></span></button></span>
                 </div>
             </div>
@@ -485,8 +335,7 @@ $TopCarousel = $response;
             }else{
                 echo "<b>Error on loading auctions: </b>" . "<br><br>" . $auctions[1];
             }
-
-
+            
             ?>
 
 
