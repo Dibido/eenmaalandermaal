501d96b77b6650ee039780adc6693ffd57b1493d
Dibido <dibrandokter@hotmail.com>
2017-05-15 14:45:22 +0200

    added logic for filter selection and price

--- resultaten.php
+++ resultaten.php
@@ -2,48 +2,119 @@
 require 'PHP/connection.php';
 require 'PHP/Functions.php';
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['zoekterm'])) {
+        $zoekterm = ($_GET['zoekterm']);
+    }
+    if (isset($_GET['sorteerfilter'])) {
+        $sorteerfilter = $_GET['sorteerfilter'];
+    }
+    if (!empty($zoekterm)) {
+        //bouwen query
+        $sql = "SELECT  *
+                        FROM Voorwerp v 
+                        LEFT JOIN Bod b ON v.VW_voorwerpnummer = b.BOD_voorwerpnummer 
+                        WHERE (B.BOD_bodbedrag = (SELECT TOP 1 BOD_Bodbedrag 
+                        FROM Bod 
+                        WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag 
+                        FROM Bod WHERE BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) OR b.BOD_bodbedrag IS NULL) 
+                        AND VW_titel LIKE '%$zoekterm%'";
+        $result = $connection->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        outputRows($result);
+    }
+}
+
+function outputRows($result)
+{
+    global $zoekterm;
+
+    foreach ($result as $row) {
+
+        DrawAuction($row);
+    }
+}
+
+
+/*
+
+$titel = $row['VW_titel'];
+$beschrijving = $row['VW_beschrijving'];
+$bodBedrag = $row['BOD_bodbedrag'];
+$tijd = $row['VW_looptijdEinde'] - $row['VW_looptijdStart'];
+
+
+echo "<div class=\"item  col-xs-4 col-lg-4\">
+<div class=\"veiling thumbnail\">
+<div class=\"veiling-titel label label-info\">
+    {$row['VW_titel']}
+</div>
+<div class=\"veiling-image\" style=\"background-image:url(images/16-9.jpeg)\"></div>
+<p>{$row['VW_beschrijving']}</p>
+<div class=\"veiling-prijs-tijd\">
+    <div class=\"prijs label label-default\"><i class=\"glyphicon glyphicon-euro\">
+                       {$row['BOD_bodbedrag']}
+    </i>
+    </div>
+    <div class=\"tijd label label-default\"> $tijd <i class=\"glyphicon glyphicon-time\"></i></div>
+</div>
+<div class=\"veiling-rating-bied label label-default\">
+    <div class=\"rating text-center\">
+        <i class=\"glyphicon glyphicon-star\"></i>
+        <i class=\"glyphicon glyphicon-star\"></i>
+        <i class=\"glyphicon glyphicon-star\"></i>
+        <i class=\"glyphicon glyphicon-star\"></i>
+        <i class=\"glyphicon glyphicon-star-empty\"></i>
+    </div>
+    <button class=\"btn btn-primary bied\">Bied Nu!</button>
+</div>
+</div>
+</div>";
+*/
 ?>
 
 <!doctype html>
 
+
 <html lang="en">
 <meta charset="utf-8">
 
-<title>EenmaalAndermaal - Beste veilingssite van Nederland</title>
-<meta name="description" content="EenmaalAndermaal">
-<meta name="author" content="Iproject - Groep 3">
+<head>
+
+    <title>EenmaalAndermaal - Beste veilingssite van Nederland</title>
+    <meta name="description" content="EenmaalAndermaal">
+    <meta name="author" content="Iproject - Groep 3">
+
+    <!-- Theme colours for mobile -->
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#F6D155">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#F6D155">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#F6D155">
 
 
-<!-- Theme colours for mobile -->
-<!-- Chrome, Firefox OS and Opera -->
-<meta name="theme-color" content="#F6D155">
-<!-- Windows Phone -->
-<meta name="msapplication-navbutton-color" content="#F6D155">
-<!-- iOS Safari -->
-<meta name="apple-mobile-web-app-status-bar-style" content="#F6D155">
+    <!-- setting the browser icon -->
+    <link rel="icon" href="images/Site-logo.png">
 
 
-<!-- setting the browser icon -->
-<link rel="icon" href="images/Site-logo.png">
+    <!-- bootstrap !-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/theme.css">
+    <link rel="stylesheet" href="CSS/BootstrapXL.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/HomePage.css">
+    <link rel="stylesheet" href="CSS/veiling.css">
+    <link rel="stylesheet" href="CSS/navigation.css">
+    <link rel="stylesheet" href="CSS/resultaten.css">
 
-<!-- bootstrap !-->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="CSS/theme.css">
-<link rel="stylesheet" href="CSS/BootstrapXL.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<!-- CSS -->
-<link rel="stylesheet" href="CSS/HomePage.css">
-<link rel="stylesheet" href="CSS/veiling.css">
-<link rel="stylesheet" href="CSS/navigation.css">
-<link rel="stylesheet" href="CSS/resultaten.css">
-
-<!-- CSS voor price slider -->
-<link rel="stylesheet" type="text/css"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/bootstrap-slider.js"></script>
+    <!-- CSS voor price slider -->
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/bootstrap-slider.js"></script>
 
 </head>
 <body>
@@ -79,7 +150,9 @@ require 'PHP/Functions.php';
             <div class="form-group" style="display:inline;">
                 <div class="input-group" style="display:table;">
                     <input class="form-control" name="zoekterm" placeholder="Search Here" autocomplete="off"
-                           autofocus="autofocus" type="text">
+                           autofocus="autofocus" type="text" value="<?php if (!empty($zoekterm)) {
+                        echo($zoekterm);
+                    } ?>">
                     <span class="input-group-addon" style="width:1%;"><span
                                 class="glyphicon glyphicon-search"></span></span>
                 </div>
@@ -107,10 +180,15 @@ require 'PHP/Functions.php';
             <div class="list-group">
                 <a href="#" class="list-group-item active">Opties</a>
 
-                <form method="post" action="/action_page_post.php">
+                <form method="get" action="resultaten.php">
+
+                    <input type="hidden" name="zoekterm" value="<?php global $zoekterm;
+                    echo($zoekterm); ?>">
+
+                    <input type="hidden" name="filter" value="<?php ?>">
 
                     <a href="#" class="list-group-item">Prijs: <b>€ 10 - € 1000</b>
-                        <input id="pslider" type="text"
+                        <input id="pslider" type="text" name="prijs"
                                class="span2" value=""
                                data-slider-min="10"
                                data-slider-max="1000"
@@ -119,13 +197,14 @@ require 'PHP/Functions.php';
                     </a>
 
 
-                    <a href="#" class="list-group-item">Rating:
-                        <select class="form-control" id="rating">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                    <a href="#" class="list-group-item">Betalingsmethode:
+                        <select class="form-control" id="betalingsmethode" name="betalingsmethode">
+                            <option>betalen jong</option>
+                            <?php
+                            /*foreach(){
+
+                            }*/
+                            ?>
                         </select>
                     </a>
 
@@ -160,13 +239,22 @@ require 'PHP/Functions.php';
 
             <div class="pull-right">
                 <b>Sorteer:</b>
-                <select id="example">
-                    <option value="1">Tijd: nieuw aangeboden</option>
-                    <option value="2">Tijd: eerst afgelopen</option>
-                    <option value="3">Prijs: laagste bovenaan</option>
-                    <option value="4">Prijs: hoogste bovenaan</option>
-                    <option value="5">Afstand: dichtstbijzijnde eerst</option>
-                </select>
+                <form method="get" action="resultaten.php">
+                    <select id="sorteerfilter" name="sorteerfilter" onchange="this.form.submit()">
+                        <?php
+                        if(isset($sorteerfilter)){
+                            global $sorteerfilter;
+                            echo('<option value="0" selected> ' . urldecode($sorteerfilter) . '</option>');
+                        }
+                        ?>
+                        <option value="Tijd: nieuw aangeboden">Tijd: nieuw aangeboden</option>
+                        <option value="Tijd: eerst afgelopen">Tijd: eerst afgelopen</option>
+                        <option value="Prijs: laagste bovenaan">Prijs: laagste bovenaan</option>
+                        <option value="Prijs: hoogste bovenaan">Prijs: hoogste bovenaan</option>
+                        <option value="Afstand: dichtstbijzijnde eerst">Afstand: dichtstbijzijnde eerst</option>
+                    </select>
+                    <noscript><input type="submit" value="aanpassen"</noscript>
+                </form>
             </div>
         </ol>
     </div>
@@ -186,80 +274,6 @@ require 'PHP/Functions.php';
                             class="glyphicon glyphicon-th"></span>Grid</a>
             </div>
         </div>
-
-        <?php
-
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            if (isset($_GET['zoekterm'])) {
-                $zoekterm = ($_GET['zoekterm']);
-                if (!empty($zoekterm)) {
-                    //bouwen query
-                    $sql = "SELECT  * 
-                        FROM Voorwerp v 
-                        LEFT JOIN Bod b ON v.VW_voorwerpnummer = b.BOD_voorwerpnummer 
-                        WHERE (B.BOD_bodbedrag = (SELECT TOP 1 BOD_Bodbedrag 
-                        FROM Bod 
-                        WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag 
-                        FROM Bod WHERE BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer ORDER BY BOD_Bodbedrag DESC) OR b.BOD_bodbedrag IS NULL) 
-                        AND VW_titel LIKE '%$zoekterm%'";
-                    $result = $connection->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                    outputRows($result);
-                }
-            }
-        }
-
-        function outputRows($result)
-        {
-            global $zoekterm;
-            if (empty($result)) {
-                echo "Geen resultaten gevonden voor: '" . $zoekterm . "'";
-            }
-
-            foreach ($result as $row) {
-
-                DrawAuction($row);
-            }
-        }
-
-
-        /*
-
-        $titel = $row['VW_titel'];
-        $beschrijving = $row['VW_beschrijving'];
-        $bodBedrag = $row['BOD_bodbedrag'];
-        $tijd = $row['VW_looptijdEinde'] - $row['VW_looptijdStart'];
-
-
-        echo "<div class=\"item  col-xs-4 col-lg-4\">
-    <div class=\"veiling thumbnail\">
-        <div class=\"veiling-titel label label-info\">
-            {$row['VW_titel']}
-        </div>
-        <div class=\"veiling-image\" style=\"background-image:url(images/16-9.jpeg)\"></div>
-        <p>{$row['VW_beschrijving']}</p>
-        <div class=\"veiling-prijs-tijd\">
-            <div class=\"prijs label label-default\"><i class=\"glyphicon glyphicon-euro\">
-                               {$row['BOD_bodbedrag']}
-            </i>
-            </div>
-            <div class=\"tijd label label-default\"> $tijd <i class=\"glyphicon glyphicon-time\"></i></div>
-        </div>
-        <div class=\"veiling-rating-bied label label-default\">
-            <div class=\"rating text-center\">
-                <i class=\"glyphicon glyphicon-star\"></i>
-                <i class=\"glyphicon glyphicon-star\"></i>
-                <i class=\"glyphicon glyphicon-star\"></i>
-                <i class=\"glyphicon glyphicon-star\"></i>
-                <i class=\"glyphicon glyphicon-star-empty\"></i>
-            </div>
-            <button class=\"btn btn-primary bied\">Bied Nu!</button>
-        </div>
-    </div>
-</div>";
-        */
-        ?>
-
-
     </div>
 </div>
 
