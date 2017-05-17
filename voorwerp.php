57293c94b54e765614c6d06963354ca555f1c826
athan88 <na.tuinstra@gmail.com>
2017-05-17 10:21:50 +0200

    voorwerp pagina

---
+++ voorwerp.php
@@ -0,0 +1,65 @@
+<?php
+
+require('PHP/connection.php');
+require('PHP/Functions.php');
+require('PHP/SQL-Queries.php');
+
+$response = NULL;
+
+?>
+
+<!doctype html>
+
+<!-- onzin comment voor pushes nr2 -->
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>EenmaalAndermaal - Beste veilingssite van Nederland</title>
+    <meta name="description" content="EenmaalAndermaal">
+    <meta name="author" content="Iproject - Groep 3">
+
+
+    <!-- Theme colours for mobile -->
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#F6D155">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#F6D155">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#F6D155">
+
+
+    <!-- setting the browser icon -->
+    <link rel="icon" href="images/Site-logo.png">
+
+
+    <!-- bootstrap !-->
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/theme.css">
+    <link rel="stylesheet" href="CSS/BootstrapXL.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/HomePage.css">
+    <link rel="stylesheet" href="CSS/veiling.css">
+    <link rel="stylesheet" href="CSS/navigation.css">
+
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+
+<?php
+require('navbar.html');
+?>
+
+
+
+</body>
