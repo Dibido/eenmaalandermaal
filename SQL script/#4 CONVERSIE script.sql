43f22630dafecce52ba964a1fea270feb782c9f7
Dibido <dibrandokter@hotmail.com>
2017-05-22 11:04:45 +0200

    removed database selectors.

--- SQL script/#4 CONVERSIE script.sql
+++ SQL script/#4 CONVERSIE script.sql
@@ -1,17 +1,17 @@
 --Conversiescript rubrieken
 BEGIN TRANSACTION
-INSERT INTO eenmaalandermaal.dbo.Rubriek
+INSERT INTO Rubriek
   SELECT
     ID     AS RB_Nummer,
     name   AS RB_naam,
     parent AS RB_parent,
     ID     AS RB_volgnummer
-  FROM veilingssite.dbo.Categorieen
+  FROM Categorieen
 COMMIT
 
 --Conversiescript Users
 BEGIN TRANSACTION
-INSERT INTO eenmaalandermaal.dbo.Gebruiker (GEB_gebruikersnaam, GEB_voornaam, GEB_achternaam, GEB_adresregel_1, GEB_geboortedag, GEB_mailbox, GEB_wachtwoord,
+INSERT INTO Gebruiker (GEB_gebruikersnaam, GEB_voornaam, GEB_achternaam, GEB_adresregel_1, GEB_geboortedag, GEB_mailbox, GEB_wachtwoord,
                                             GEB_vraag, GEB_antwoordtekst, GEB_postcode, GEB_plaatsnaam, GEB_Land, GEB_Rating)
   SELECT DISTINCT
     LTRIM(RTRIM(Username))   AS GEB_gebruikersnaam,
@@ -32,12 +32,12 @@ INSERT INTO eenmaalandermaal.dbo.Gebruiker (GEB_gebruikersnaam, GEB_voornaam, GE
      FROM veilingssite.dbo.Users u
      WHERE u.Username = Username
      ORDER BY Rating DESC)   AS GEB_Rating
-  FROM veilingssite.dbo.Users
+  FROM Users
 COMMIT
 
 --Conversiescript Items
 BEGIN TRANSACTION
-INSERT INTO eenmaalandermaal.dbo.Voorwerp (VW_voorwerpnummer, VW_titel, VW_beschrijving, VW_land, VW_verkoper, VW_conditie, VW_thumbnail)
+INSERT INTO Voorwerp (VW_voorwerpnummer, VW_titel, VW_beschrijving, VW_land, VW_verkoper, VW_conditie, VW_thumbnail)
   SELECT
     ID           AS VW_voorwerpnummer,
     Title        AS VW_titel,
@@ -47,26 +47,26 @@ INSERT INTO eenmaalandermaal.dbo.Voorwerp (VW_voorwerpnummer, VW_titel, VW_besch
     Verkoper     AS VW_verkoper,
     Conditie     AS VW_conditie,
     Thumbnail    AS VW_thumbnail
-  FROM veilingssite.dbo.Items
+  FROM Items
 
-INSERT INTO eenmaalandermaal.dbo.Bod (BOD_voorwerpnummer, BOD_bodbedrag, BOD_gebruiker, BOD_bodTijdEnDag)
+INSERT INTO Bod (BOD_voorwerpnummer, BOD_bodbedrag, BOD_gebruiker, BOD_bodTijdEnDag)
   SELECT
     ID        AS BOD_voorwerpnummer,
     Prijs     AS BOD_bodbedrag,
     --Prijs omzetten naar euro aan de hand van een wisselkoers
     Verkoper  AS BOD_gebruiker,
     getdate() AS BOD_bodTijdEnDag
-  FROM veilingssite.dbo.Items
+  FROM Items
 
-INSERT INTO eenmaalandermaal.dbo.Gebruiker (GEB_postcode)
+INSERT INTO Gebruiker (GEB_postcode)
   SELECT Postcode AS GEB_postcode
-  FROM veilingssite.dbo.Items
+  FROM Items
 
-INSERT INTO eenmaalandermaal.dbo.Voorwerp_Rubriek
+INSERT INTO Voorwerp_Rubriek
   SELECT
     Categorie AS VR_Voorwerp_Nummer,
     ID        AS VR_Rubriek_Nummer
-  FROM veilingssite.dbo.Items
+  FROM Items
 COMMIT
 
 
@@ -88,7 +88,7 @@ INSERT INTO Bestand (BES_filenaam, BES_voorwerpnummer)
            Rank()
            OVER ( PARTITION BY ItemID --OVER voegt informatie samen zonder te groeperen
              ORDER BY IllustratieFile DESC ) AS Rank
-         FROM veilingssite.dbo.Illustraties
+         FROM Illustraties
        ) rs
   WHERE Rank <= 3 --Selecteerd de top 3
         AND EXISTS(SELECT * --Als het voorwerp bestaat.
