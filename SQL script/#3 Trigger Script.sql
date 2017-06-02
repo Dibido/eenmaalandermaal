18c8bd913fc859132e4daea15ba2eebb5bd9b013
Dibido <dibrandokter@hotmail.com>
2017-06-02 15:27:05 +0200

    hoogstebod trigger aangemaakt.

--- SQL script/#3 Trigger Script.sql
+++ SQL script/#3 Trigger Script.sql
@@ -1,9 +1,32 @@
+--Trigger om het hoogste bod op een voorwerp bij te houden
+IF OBJECT_ID('TR_HoogsteBod') IS NOT NULL
+  DROP TRIGGER [dbo].[TR_HoogsteBod]
+GO
+CREATE TRIGGER TR_HoogsteBod
+  ON Bod
+FOR INSERT, UPDATE, DELETE
+AS
+  BEGIN
+    --Bijwerken hoogste bodprijs voorwerp.
+    UPDATE Voorwerp
+    SET VW_hoogstebod = (COALESCE((SELECT TOP 1 BOD_Bodbedrag
+                                   FROM Bod
+                                   WHERE BOD_Bodbedrag IN (SELECT TOP 1 BOD_Bodbedrag
+                                                           FROM Bod
+                                                           WHERE BOD_voorwerpnummer = VW_voorwerpnummer
+                                                           ORDER BY BOD_Bodbedrag DESC) AND
+                                         BOD_voorwerpnummer = VW_voorwerpnummer
+                                   ORDER BY BOD_Bodbedrag DESC), VW_startprijs
+    ))
+  END
+
 --Trigger om het aantal voorwerpen per rubriek te berekenen.
 IF OBJECT_ID('TR_ComputedCount') IS NOT NULL
   DROP TRIGGER [dbo].[TR_ComputedCount]
 GO
-CREATE TRIGGER TR_ComputedCount ON Voorwerp_Rubriek
-FOR INSERT,UPDATE,DELETE
+CREATE TRIGGER TR_ComputedCount
+  ON Voorwerp_Rubriek
+FOR INSERT, UPDATE, DELETE
 AS
   BEGIN
     --Bijwerken aantal voorwerpen per categorie
@@ -11,11 +34,13 @@ AS
     UPDATE Rubriek
     SET RB_voorwerpcount =
     (
-      SELECT COUNT(*) FROM Voorwerp_Rubriek
-      WHERE Voorwerp_Rubriek.VR_Rubriek_Nummer=RB_Nummer
+      SELECT COUNT(*)
+      FROM Voorwerp_Rubriek
+      WHERE Voorwerp_Rubriek.VR_Rubriek_Nummer = RB_Nummer
     )
-    WHERE EXISTS(SELECT * FROM Voorwerp_Rubriek
-    WHERE Voorwerp_Rubriek.VR_Rubriek_Nummer = RB_Nummer)
+    WHERE EXISTS(SELECT *
+                 FROM Voorwerp_Rubriek
+                 WHERE Voorwerp_Rubriek.VR_Rubriek_Nummer = RB_Nummer)
   END
 
 -- Trigger die registratiecodes die ouder dan 24 uur zijn verwijderd.
@@ -29,6 +54,6 @@ FOR INSERT, UPDATE
 AS
   BEGIN
     DELETE FROM Registreer
-    WHERE  REG_tijd < DATEADD(day, -1, GETDATE())
+    WHERE REG_tijd < DATEADD(DAY, -1, GETDATE())
   END
 GO
