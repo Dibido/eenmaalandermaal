3065ce361451dcb3af57e359b59957a4701e9229
TESTERZZ <djibrahim@live.nl>
2017-06-06 15:01:34 +0200

    Profiel pagina html

--- profiel.php
+++ profiel.php
@@ -3,7 +3,7 @@ session_start();
 
 require('PHP/Functions.php');
 require('PHP/SQL-Queries.php');
-//require('PHP/connection.php');
+require('PHP/connection.php');
 
 //$ItemInfo = GetItemDetails(fonorama);
 
@@ -72,6 +72,9 @@ require('navbar.php');
                         <p><span class="glyphicon glyphicon-globe"></span> Land:</p>
                         <p><span class="glyphicon glyphicon-map-marker"></span> Plaats:</p>
                         <p><span class="glyphicon glyphicon-shopping-cart"></span> Type account:</p>
+                        <br>
+                        <br>
+                        <br>
                     </div>
         </div>
     </div>
@@ -85,9 +88,9 @@ require('navbar.php');
             <div class="list-group-item active text-center">Gegevens</div>
             <div class="panel-body">
 
-               <div class="list-group-item "><span class="glyphicon glyphicon-user"></span> Naam:</div>
+               <div class="list-group-item "><span class="glyphicon glyphicon-user"></span> Naam: <span class="glyphicon glyphicon-user text-right" style="font-size: 75px;"></span></div>
                <div class="list-group-item"><span class="glyphicon glyphicon-shopping-cart"></span> Type account:</div>
-               <a href="#"><button class="btn btn-primary center-block btn-lg " type="button">
+               <a href="#"><button class="btn btn-primary center-block btn-lg " type="button" style="margin-top: 10px;">
                             <span class="glyphicon glyphicon-repeat"></span> UPGRADE ACCOUNT!</button></a>
                
                 
@@ -121,48 +124,35 @@ require('navbar.php');
         </div>
     </div>
 
-            
-
-
-
-     
 
 <!-- ADS -->
-
-    <div class="col-md-12 col-sm-12">
-    
-        <div class="panel panel-default Details-wrapper">
-            <div class="list-group-item active text-center">Persoonlijke Beschrijving</div>
+<div class="col-md-12 col-sm-12">
+    <div class="panel panel-default Details-wrapper">
+        <div class="list-group-item active text-center">Mijn EenMaalAndermaal</div>
  
-  <ul class="nav nav-pills">
-    <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
-    <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
-    <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
-    <li><a data-toggle="pill" href="#menu3">Menu 3</a></li>
+<div class="panel-body">
+
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Mijn advertenties</a></li>
+    <li><a data-toggle="tab" href="#menu1">Mijn biedingen</a></li>
+    <li><a data-toggle="tab" href="#menu2">Gewonnen veilingen</a></li>
   </ul>
-  
+
   <div class="tab-content">
     <div id="home" class="tab-pane fade in active">
-      <h3>HOME</h3>
+      <h3>Mijn advertenties</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
     <div id="menu1" class="tab-pane fade">
-      <h3>Menu 1</h3>
+      <h3>Mijn biedingen</h3>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
     <div id="menu2" class="tab-pane fade">
-      <h3>Menu 2</h3>
+      <h3>Gewonnen veilingen</h3>
       <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-    </div>
-    <div id="menu3" class="tab-pane fade">
-      <h3>Menu 3</h3>
-      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
     </div>
   </div>
 </div>
-
-
-
         </div>
     </div>
 
