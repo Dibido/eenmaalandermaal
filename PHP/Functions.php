98158bf51b8e5749b1a34a9414e011ce79f2a7bb
Dibido <dibrandokter@hotmail.com>
2017-06-07 23:41:57 +0200

    added id in variable to make it unique

--- PHP/Functions.php
+++ PHP/Functions.php
@@ -911,37 +911,31 @@ function drawPagenumbers($pagenum,$Dictionary,$result,$ResultsPerPage,$zoekterm,
 
 function createTimer($tijd, $VW_Titel, $VW_Nummer)
 {
+    $timer = "timer".$VW_Nummer;
     echo '<script>
     // Set the date we\'re counting down to
-    var countDownDate = new Date("' . $tijd . '").getTime();
-
+    var '.$timer.' = new Date("' . $tijd . '").getTime();
     // Update the count down every 1 second
     var x = setInterval(function() {
-
         // Get todays date and time
         var now = new Date().getTime();
-
         // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-
+        var distance = '.$timer.' - now;
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
         // Display the result in the element with id="demo"
-     
-     
-     
-       if(days >= 1){
+        
+        if(days >= 1){
         document.getElementById("' . $VW_Nummer . '").innerHTML = days + "d " + hours + "h "
             + minutes + "m " ;
-        }else if(seconds < 10){
-        document.getElementById("' . $VW_Nummer . '").innerHTML =  hours + "h " 
+        }else if(days < 1 && seconds < 10){
+        document.getElementById("' . $VW_Nummer . '").innerHTML = hours + "h "
             + minutes + "m " + "0" + seconds + "s" ;
         }else{
-        document.getElementById("' . $VW_Nummer . '").innerHTML =  hours + "h " 
+        document.getElementById("' . $VW_Nummer . '").innerHTML = hours + "h "
             + minutes + "m " + seconds +  "s" ;
         }
         // If the count down is finished, write some text
