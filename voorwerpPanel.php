961b10aa66869c2a78388dcd90d9c6da30ea240c
Luck Matheus Hermsen <luckmatheushermsen@hotmail.com>
2017-06-07 09:57:36 +0200

    changes to rating on voorwerpPanel.php

--- voorwerpPanel.php
+++ voorwerpPanel.php
@@ -34,65 +34,64 @@ session_start();
 
 <body>
 
-    <div class="panel panel-default">
+<div class="panel panel-default">
 
 
-        <!-- dynamically loading the price -->
+    <!-- dynamically loading the price -->
 
-        <div class="panel-heading text-center">Prijs</div>
-        <div class="panel-body">
+    <div class="panel-heading text-center">Prijs</div>
+    <div class="panel-body">
             <span id="Price" class="text-center"><i
                         class="glyphicon glyphicon-euro"></i><?php echo " " . $ItemInfo["prijs"]; ?></span>
-        </div>
+    </div>
 
-        <!-- dynamically loading the offers -->
+    <!-- dynamically loading the offers -->
 
-        <div class="panel-heading text-center">Recente biedingen</div>
-        <div class="panel-body">
-            <?php
-
-
-
-            $LastOffers = GetLastOffers($ItemInfo["VW_voorwerpnummer"]);
-
-            if (!isset($LastOffers[0]) or empty($LastOffers[0])) {
-                echo "<div class=\"OldOffer\">Wees de eerste om een bod uit te brengen</div>";
-                echo "</div>";
-            } else {
-                $Bod = 0;
-                foreach ($LastOffers as $lastOffer) {
-                    $Bod++;
-                    $Bodtijd = ConvertTime($lastOffer["BOD_bodTijdEnDag"]);
+    <div class="panel-heading text-center">Recente biedingen</div>
+    <div class="panel-body">
+        <?php
 
 
-                    // build the surrounding div for the offers
-                    echo "<div class=\"OldOffer\"";
+        $LastOffers = GetLastOffers($ItemInfo["VW_voorwerpnummer"]);
 
-                    //border-bottom: #e5e5e5 solid 2px;
+        if (!isset($LastOffers[0]) or empty($LastOffers[0])) {
+            echo "<div class=\"OldOffer\">Wees de eerste om een bod uit te brengen</div>";
+            echo "</div>";
+        } else {
+            $Bod = 0;
+            foreach ($LastOffers as $lastOffer) {
+                $Bod++;
+                $Bodtijd = ConvertTime($lastOffer["BOD_bodTijdEnDag"]);
 
-                    //checking if the offer is owned by the current user
-                    if($_SESSION["Username"] == $lastOffer["BOD_gebruiker"]) {
-                        echo "style=\"border-bottom: #524BAB solid 2px; \" ";
-                    }
 
-                    //building the inside of the offer
-                    echo "><div class=\"OldOfferUserName\">" . $Bod . '. ' . $lastOffer["BOD_gebruiker"] . "</div><div class=\"OldOfferPrice\">" . $Bodtijd . "</div><div class=\"OldOfferPrice\">&euro;" . $lastOffer["BOD_bodbedrag"] . "</div></div>";
+                // build the surrounding div for the offers
+                echo "<div class=\"OldOffer\"";
 
-                    if ($Bod == 4) {
-                        echo "<div id=\"MoreOffers\" class=\"collapse\">";
-                    }
+                //border-bottom: #e5e5e5 solid 2px;
+
+                //checking if the offer is owned by the current user
+                if ($_SESSION["Username"] == $lastOffer["BOD_gebruiker"]) {
+                    echo "style=\"border-bottom: #524BAB solid 2px; \" ";
                 }
 
-                echo "</div>";
+                //building the inside of the offer
+                echo "><div class=\"OldOfferUserName\">" . $Bod . '. ' . $lastOffer["BOD_gebruiker"] . "</div><div class=\"OldOfferPrice\">" . $Bodtijd . "</div><div class=\"OldOfferPrice\">&euro;" . $lastOffer["BOD_bodbedrag"] . "</div></div>";
 
-                if($Bod >= 4){
-                    echo "<button data-toggle=\"collapse\" data-target=\"#MoreOffers\" class=\"btn btn-default MoreOffers collapsed\" value=\"Meer biedingen / Minder biedingen\">Meer / Minder biedingen</button>";
-                    echo "</div>";
+                if ($Bod == 4) {
+                    echo "<div id=\"MoreOffers\" class=\"collapse\">";
                 }
             }
 
+            echo "</div>";
 
-            ?>
+            if ($Bod >= 4) {
+                echo "<button data-toggle=\"collapse\" data-target=\"#MoreOffers\" class=\"btn btn-default MoreOffers collapsed\" value=\"Meer biedingen / Minder biedingen\">Meer / Minder biedingen</button>";
+                echo "</div>";
+            }
+        }
+
+
+        ?>
 
 
         <!-- Biedknop -->
@@ -102,14 +101,15 @@ session_start();
             <form id="bodForm" class="form-inline" method="POST" action="voorwerp.php?ItemID=<?php echo $ItemID; ?>">
                 <div class="input-group InputBod">
                     <div class="input-group-addon">&euro;</div>
-                    <input form="bodForm" type="number" step=".01" class="form-control" id="bodInput" name="bod" min="<?php echo $minimumBod; ?>" max="999999999.99" autofocus>
+                    <input form="bodForm" type="number" step=".01" class="form-control" id="bodInput" name="bod"
+                           min="<?php echo $minimumBod; ?>" max="999999999.99" autofocus>
                 </div>
                 <button type="submit" class="btn btn-primary SubmitButton">Bied</button>
             </form>
         </div>
 
 
-         <!-- gebruikersinformatie -->
+        <!-- gebruikersinformatie -->
 
         <div class="panel-heading text-center">Gebruikers informatie</div>
         <div class="panel-body">
@@ -122,10 +122,10 @@ session_start();
                     <?php
 
                     $Userinfo = GetUserInfoPerAuction($ItemInfo["VW_verkoper"]);
-
+                    print_r($Userinfo);
                     $rating = floor($Userinfo[0]["GEB_rating"]) / 10;
 
-                    for($i = 0; $i < $rating/2; $i++){
+                    for ($i = 0; $i < $rating / 2; $i++) {
                         echo " <i class=\"glyphicon glyphicon-star\"></i>";
                     }
 
@@ -136,12 +136,6 @@ session_start();
         </div>
 
     </div>
-
-
-
-
-
-
 
 
 </body>
