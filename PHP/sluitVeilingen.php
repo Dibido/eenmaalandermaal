0443d1c89cdc39c983bc16866394e2e93ec4001e
Luck Matheus Hermsen <luckmatheushermsen@hotmail.com>
2017-06-09 16:23:15 +0200

    Changes to sluitVeilingen.php

--- PHP/sluitVeilingen.php
+++ PHP/sluitVeilingen.php
@@ -45,7 +45,7 @@ foreach ($info as $GeslotenVeiling) {
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= 'From: info@iproject3.icasites.nl' . "\r\n";
-    $subject = 'Uw heeft een veiling gewonnen!';
+    $subject = 'U heeft een veiling gewonnen!';
     $message = 'test';
 
     $message = '
