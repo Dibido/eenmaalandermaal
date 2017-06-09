b69bfa04bda8f776985eb0a0e0f01b678ce0e937
Dibido <dibrandokter@hotmail.com>
2017-06-09 15:39:46 +0200

    formatted code.

--- profiel.php
+++ profiel.php
@@ -113,25 +113,6 @@ require('navbar.php');
                                     echo " <i class=\"glyphicon glyphicon-star-empty\"></i>";
                                 }
                                 //TODO: aantal sterren uitrekenen van range 0.0 - 100.0
-                <div class="list-group-item "><span class="glyphicon glyphicon-user"></span> Username:
-                    <b><?php echo "$username" ?> </b></div>
-                <div class="list-group-item"><span class="glyphicon glyphicon-shopping-cart"></span> Type account:
-                    <b><?php if ($userinfo["GEB_verkoper"] == 0) {
-                            echo "Geen verkoper";
-                        } else echo "Verkoper" ?></b></div>
-                <div class="list-group-item "><span class="glyphicon glyphicon-thumbs-up"></span> Mijn Rating: <b><?php
-
-                        $Userinfo = GetUserInfoPerAuction($username);
-                        $rating = ceil(($Userinfo[0]["GEB_rating"] / 20)); // Delen door 2. Rating van 0 tot 100, sterren 0 tot 5.
-                        $legeSterren = 5 - $rating;
-                        for ($i = 0; $i < $rating; $i++) {
-                            echo " <i class=\"glyphicon glyphicon-star\"></i>";
-                        }
-                        for ($i = 0; $i < $legeSterren; $i++) {
-                            echo " <i class=\"glyphicon glyphicon-star-empty\"></i>";
-                        }
-                        //TODO: aantal sterren uitrekenen van range 0.0 - 100.0
-
                     */
 
 
