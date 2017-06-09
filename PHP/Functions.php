d4bcb8b112d6c9345da7291a2efb4aa313732fa5
athan88 <na.tuinstra@gmail.com>
2017-06-09 14:07:28 +0200

    Functions update v2

--- PHP/Functions.php
+++ PHP/Functions.php
@@ -131,7 +131,7 @@ SELECT
   VW_verzendkosten,
   VW_conditie, 
   VW_hoogstebod,
-  VW_minimalenieuwebod
+  VW_minimalenieuwebod,
   
 FROM Voorwerp
   FULL OUTER JOIN Bod ON Bod.BOD_voorwerpnummer = Voorwerp.VW_voorwerpnummer
@@ -1472,7 +1472,7 @@ function FindUser($username)
  */
 
 
-
+/*
 function upgradeAccount($itemID, $user, $offer)
 {
     GLOBAL $connection;
@@ -1491,10 +1491,9 @@ EOT;
     $stmt->execute();
 
 }
-
+*/
 function createUpgradeCode($username)
 {
-
 
     /* preparing the query and inserting into the database */
     GLOBAL $connection;
@@ -1510,16 +1509,23 @@ VALUES (:username , :code, :tijd )
 
 EOT;
 
-
-
     $stmt = $connection->prepare($query);
-    $stmt->bindParam(':offer', $username);
-    $stmt->bindParam(':user', $code);
-    $stmt->bindParam(':itemID', $date);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':code', $code);
+    $stmt->bindParam(':tijd', $date);
     $stmt->execute();
 
+}
 
 
+
+
+
+
+
+
+function sendUpgradeMail($username)
+{
 
     /* preparing the mail */
 
@@ -1537,12 +1543,12 @@ EOT;
 SELECT GEB_mailbox FROM Gebruiker WHERE GEB_gebruikersnaam = ?
 
 EOT;
+    GLOBAL $connection;
 
     $stmt = $connection->prepare($query);
     $stmt->execute(array($username));
-    $stmt->fetch();
-
-    $email = $stmt[0];
+    $email = $stmt->fetch();
+    $email = $email[0];
 
 
     //Verificatie mail
@@ -1693,6 +1699,8 @@ EOT;
 ';
 
     mail($email, $subject, $message, $headers);
+
+
 
 }
 
