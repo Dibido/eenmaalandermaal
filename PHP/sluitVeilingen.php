6bce71d96b5167e62587baefc4141396c6e1359b
Luck Matheus Hermsen <luckmatheushermsen@hotmail.com>
2017-06-09 15:09:09 +0200

    Changes ro sluitVeilingen.php

--- PHP/sluitVeilingen.php
+++ PHP/sluitVeilingen.php
@@ -140,17 +140,17 @@ foreach ($info as $GeslotenVeiling) {
                   <tr>
                     <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Beste gebruiker,</p>
                     <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">U heeft de veiling met de titel: <b> ' . $titel . ' </b> gewonnen!</p>
-                    <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">U heeft voor dit object €<b> ' . $verkoopprijs . ' ,-</b> betaald.</p>
+                    <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">U heeft voor dit object �<b> ' . $verkoopprijs . ' </b> betaald.</p>
                   </tr>
                 </table>
                 <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Klik op de knop om de veiling te bekijken.</p>
                 <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;box-sizing:border-box;width:100%;">
                   <tbody>
-                    
+                    <tr>
                       <td align="left" style="font-family:sans-serif;font-size:14px;vertical-align:top;padding-bottom:15px;">
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;width:auto;">
                           <tbody>
-                           
+                           <tr>
                               <td style="font-family:sans-serif;font-size:14px;vertical-align:top;background-color:#ffffff;border-radius:5px;text-align:center;background-color:#413b88;"> <a href="http://iproject3.icasites.nl/voorwerp.php?ItemID=' . $voorwerpnummer . ' " target="_blank" style="text-decoration:underline;background-color:#ffffff;border:solid 1px #413b88;border-radius:5px;box-sizing:border-box;color:#413b88;cursor:pointer;display:inline-block;font-size:14px;font-weight:bold;margin:0;padding:12px 25px;text-decoration:none;text-transform:capitalize;background-color:#413b88;border-color:#413b88;color:#ffffff;">Bekijken</a>
                               </td>
                             </tr>
@@ -312,11 +312,11 @@ foreach ($info as $GeslotenVeiling) {
                 <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Klik op de knop om de veiling te bekijken.</p>
                 <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;box-sizing:border-box;width:100%;">
                   <tbody>
-                    
+                    <tr>
                       <td align="left" style="font-family:sans-serif;font-size:14px;vertical-align:top;padding-bottom:15px;">
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;width:auto;">
                           <tbody>
-                           
+                           <tr>
                               <td style="font-family:sans-serif;font-size:14px;vertical-align:top;background-color:#ffffff;border-radius:5px;text-align:center;background-color:#413b88;"> <a href="http://iproject3.icasites.nl/voorwerp.php?ItemID=' . $voorwerpnummer . ' " target="_blank" style="text-decoration:underline;background-color:#ffffff;border:solid 1px #413b88;border-radius:5px;box-sizing:border-box;color:#413b88;cursor:pointer;display:inline-block;font-size:14px;font-weight:bold;margin:0;padding:12px 25px;text-decoration:none;text-transform:capitalize;background-color:#413b88;border-color:#413b88;color:#ffffff;">Bekijken</a>
                               </td>
                             </tr>
