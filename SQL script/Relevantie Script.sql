eb99a66f1a6d3726ad8a03d2e033fec26fecdcea
Jarco <jarcodavelaar@gmail.com>
2017-06-12 13:15:21 +0200

    Comments verEngelst bij relevantie script

--- SQL script/Relevantie Script.sql
+++ SQL script/Relevantie Script.sql
@@ -1,27 +1,27 @@
---Laat het aantal biedingen zien per categorie
+--Shows the amount of bid for each category.
 SELECT
-  --Selecteer de top hoeveelheid categoriën die moet worden laten zien
+  --Selects the best categories to be shown.
   TOP 10
   RB_Naam,
   SUM(tweede.aantal) AS AantalBiedingenPerCategorie,
   RB_Nummer
 FROM Rubriek
-  --Lees de de Innerjoins van achter naar voren(onder naar boven). Begin bij STAP 1 op regel 23.
-  --STAP 4: De overgebleven Rubrieken worden nog een keer gekoppeld aan hun parent zodat de Hoogste parent verkregen is.
+  --Reads the joints from the back to the front(down to up). Starts at STAP 1 on row 23.
+  --STAP 4: The categories that are left behind will be joined with the parent so we get the highest parent.
   INNER JOIN
   (SELECT
      Rubriek.RB_Parent,
      SUM(eerste.aantal) AS aantal
    FROM Rubriek
-     --STAP 3: De geselecteerde rubrieken met daarbij de aantallen per rubriek worden gekoppeld aan de eerste parent
+     --STAP 3: The selected column with the number for each column gets joined with the first parent.
      INNER JOIN (SELECT
                    RB_Parent,
                    COUNT(BOD_voorwerpnummer) AS aantal
                  FROM Voorwerp_Rubriek
-                   --STAP 2: Het Rubriek nummer wordt gekoppeld aan zijn rij in de tabel Rubriek
+                   --STAP 2: The column number gets joined with the row in the rubriek table.
                    INNER JOIN Rubriek
                      ON Rubriek.RB_Nummer = Voorwerp_Rubriek.VR_Rubriek_Nummer
-                   --STAP 1: Biedingen worden gekoppeld aan een RubriekNummer
+                   --STAP 1: Bids get joint with rubriek.
                    INNER JOIN Bod
                      ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
                  GROUP BY RB_Parent)
@@ -32,13 +32,13 @@ GROUP BY Rubriek.RB_Naam, Rubriek.RB_Nummer
 ORDER BY MAX(aantal) DESC
 
 
---Veilingen die bijna aflopen uit de belangrijkste
+--Auctions that almost expire need to be taken out of "important'.
 SELECT
-  --Vul hier je TOP X hoeveelheid in
+  --Fill your top X value in.
   TOP 2
   VW_voorwerpnummer,
   VW_titel,
-  --Laat het hoogste bod zien op het voorwerpnummer
+  --Shows the highest bid on voorwerpnummer.
   (SELECT TOP 1 BOD_Bodbedrag
    FROM Bod
    WHERE BOD_Bodbedrag NOT IN (SELECT TOP 1 BOD_Bodbedrag
@@ -46,7 +46,7 @@ SELECT
                                WHERE BOD_voorwerpnummer = VW_voorwerpnummer
                                ORDER BY BOD_Bodbedrag DESC) AND BOD_voorwerpnummer = VW_voorwerpnummer
    ORDER BY BOD_Bodbedrag DESC)                  AS prijs,
-  --Tijdsverschil tussen nu en het einde van de veiling
+  --Time difference between now and the end of an auction.
   DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde)    AS tijd,
   COUNT(*)                                       AS Biedingen,
   --Selecteerd het eerste filepath die hij vind voor het voorwerpnummer
@@ -56,29 +56,29 @@ SELECT
 FROM Voorwerp
   INNER JOIN BOD
     ON Voorwerp.VW_voorwerpnummer = BOD_voorwerpnummer
---Vul hier de minimum en maximum tijd over in
+--You can fill the max and min time in.
 WHERE DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde) < 1000 AND DATEDIFF(HOUR, GETDATE(), VW_looptijdEinde) > 2 AND
       VW_voorwerpnummer IN (SELECT BOD_voorwerpnummer
                             FROM Rubriek
-                              --Lees de de Innerjoins van achter naar voren(onder naar boven). Begin bij STAP 1 op regel 81.
-                              --STAP 4: De overgebleven Rubrieken worden nog een keer gekoppeld aan hun parent zodat de Hoogste parent verkregen is.
+                              --Read the innerjoins backwards (low to high). Start at STAP 1 on row 81.
+                              --STAP 4: The leftover rubric gets joint with their parent so we get the highest parent.
                               INNER JOIN
                               (SELECT
                                  Rubriek.RB_Parent,
                                  BOD_voorwerpnummer,
                                  aantal
-                               --STAP 3: De geselecteerde rubrieken met daarbij de aantallen per rubriek worden gekoppeld aan de eerste parent
+                               --STAP 3: The selected rubric with their numbers are added to the highest rubric.
                                FROM Rubriek
                                  INNER JOIN (SELECT
                                                RB_Parent,
                                                COUNT(BOD_voorwerpnummer) AS aantal,
                                                BOD_voorwerpnummer
                                              FROM Voorwerp_Rubriek
-                                               --STAP 2: Het Rubriek nummer wordt gekoppeld aan zijn rij in de tabel Rubriek
+                                               --STAP 2: The rubric number gets joined with the row in the rubric table.
                                                INNER JOIN Rubriek
                                                  ON Rubriek.RB_Nummer =
                                                     Voorwerp_Rubriek.VR_Rubriek_Nummer
-                                               --STAP 1: Biedingen worden gekoppeld aan een RubriekNummer
+                                               --STAP 1: Bids get joined with rubrieknummer.
                                                INNER JOIN Bod
                                                  ON Voorwerp_Rubriek.VR_Voorwerp_Nummer =
                                                     Bod.BOD_voorwerpnummer
@@ -93,12 +93,11 @@ ORDER BY Biedingen DESC,tijd ASC
 
 
 
---Nieuwe advertenties met gebruikers die een goede beoordeling hebben
+--New auctions with users that have a high rating.
 
 --TODO
 
---Belangrijkste Categorie met daarbij de gebruikers met de hoogste rating en daarna meeste biedingen per voorwerp
-
+--Most important catergory with the higest rated users and most bids for each product.
 
 SELECT
   TOP 3
@@ -117,13 +116,13 @@ SELECT
    FROM Bestand
    WHERE BES_voorwerpnummer = VW_voorwerpnummer) AS ImagePath
 FROM Voorwerp
-  --Inner join naar Bod zodat per Voorwerp het aantal biedingen bekeken kan worden.
+  --Inner join between bod and Voorwerp so we can calculate the amount of bids.
   INNER JOIN Bod
     ON Bod.BOD_voorwerpnummer = Voorwerp.VW_voorwerpnummer
---Where statement om te kijken of het Voorwerp nummer in de belangrijkste categorie zit.
+--Where statement to check if voorwerp nummer is in the most important category.
 WHERE VW_voorwerpnummer IN (
   SELECT DISTINCT BOD_voorwerpnummer
-  --Selecteerd de naam van de Hoofdcategorie per voorwerpnummer
+  --selects the name of the hoofdrubriek for each voorwerpnummer.
   FROM Bod
     INNER JOIN Voorwerp_Rubriek
       ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
@@ -134,27 +133,27 @@ WHERE VW_voorwerpnummer IN (
     INNER JOIN Rubriek r2
       ON r2.RB_Nummer = r1.RB_Parent
   WHERE r2.RB_Naam != 'root' AND r2.RB_Naam IN (
-    --Lees de de Innerjoins van achter naar voren(onder naar boven). Begin bij STAP 1 op regel 157.
-    --STAP 5: Selecteerd de parent met het hoogste aantal.
+    --read the inner joins from low to high. Start at STAP 1 on row 157.
+    --STAP 5: Selects the parent with the highest amount.
     SELECT
       TOP 1 RB_Naam
     FROM Rubriek
-      --STAP 4: De overgebleven Rubrieken worden nog een keer gekoppeld aan hun parent zodat de Hoogste parent verkregen is.
+      --STAP 4: The leftover rubrics get joined with their parent so we get the highest parent.
       INNER JOIN
       (SELECT
          Rubriek.RB_Parent,
          aantal
        FROM Rubriek
          INNER JOIN
-         --STAP 3: De geselecteerde rubrieken met daarbij de aantallen per rubriek worden gekoppeld aan de eerste parent
+         --STAP 3: The selected rubric with the number for each rubric so we can join it with the first parent.
          (SELECT
             RB_Parent,
             COUNT(BOD_voorwerpnummer) AS aantal
           FROM Voorwerp_Rubriek
-            --STAP 2: Het Rubriek nummer wordt gekoppeld aan zijn rij in de tabel Rubriek
+            --STAP 2: The rubric number gets joined with it's row in the table rubric.
             INNER JOIN Rubriek
               ON Rubriek.RB_Nummer = Voorwerp_Rubriek.VR_Rubriek_Nummer
-            --STAP 1: Voorwerpnummers worden gekoppeld aan de biedingen in de tabel Bod
+            --STAP 1: Voorwerpnummers gets joined with the bids in the Bod table.
             INNER JOIN Bod
               ON Voorwerp_Rubriek.VR_Voorwerp_Nummer = Bod.BOD_voorwerpnummer
           GROUP BY RB_Parent)
